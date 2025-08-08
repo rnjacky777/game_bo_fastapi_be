9fd25076d4e292f5951ae2d747e5f80e8e0a05a7
rnjacky777 <rnjacky777@gmail.com>
2025-08-09 00:51:27 +0800

    Add temp api

--- routers/userO.py
+++ routers/userO.py
@@ -11,3 +11,37 @@ router = APIRouter()
 def get_all_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
     return users
+
+
+# region need refactor
+from pydantic import BaseModel
+from typing import Optional
+
+class CharTempCreate(BaseModel):
+    name: str
+    rarity: int
+    description: Optional[str] = None
+    image_sm_url: Optional[str] = None
+    image_lg_url: Optional[str] = None
+    base_hp: int
+    base_mp: int
+    base_atk: int
+    base_spd: int
+    base_def: int
+
+from sqlalchemy.orm import Session
+from core_system.models import CharTemp
+from schemas.user import CharTempCreate
+
+def create_char_temp(db: Session, char_data: CharTempCreate) -> CharTemp:
+    char = CharTemp(**char_data.dict())
+    db.add(char)
+    db.commit()
+    db.refresh(char)
+    return char
+
+@router.post("/create")
+def create_char_template(char_data: CharTempCreate, db: Session = Depends(get_db)):
+    return create_char_temp(db, char_data)
+
+# endregion
